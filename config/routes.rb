6be126f8f36e5ae4bbd2e6133a5c0b235d0180ca
Tristan Boizard <boiz.tristan@gmail.com>
2019-05-15 21:10:43 +0200

    exodone

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create, :destroy]
+  end
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
